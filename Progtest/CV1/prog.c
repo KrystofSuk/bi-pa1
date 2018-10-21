e8bd3782a3438bf8ccb689f9b8fa273972a69ed1
Kryštof Šuk <suk.krystof@gmail.com>
2018-10-21 19:44:48 +0200

    CV1

---
+++ Progtest/CV1/prog.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int inp;
+	printf("ml' nob:\n");
+	if(scanf("%d",&inp)!=1){
+		printf("luj\n");
+	}else{
+		switch(inp){
+			case 1:
+				printf("Qapla'\nnoH QapmeH wo' Qaw'lu'chugh yay chavbe'lu' 'ej wo' choqmeH may' DoHlu'chugh lujbe'lu'.\n");
+				break;
+			case 2:
+				printf("Qapla'\nQu' buSHa'chugh SuvwI', batlhHa' vangchugh, qoj matlhHa'chugh, pagh ghaH SuvwI''e'.\n");
+				break;
+			case 3:
+				printf("Qapla'\nqaStaHvIS wa' ram loS SaD Hugh SIjlaH qetbogh loD.\n");
+				break;
+			case 4:
+				printf("Qapla'\nHa'DIbaH DaSop 'e' DaHechbe'chugh yIHoHQo'.\n");
+				break;
+			case 5:
+				printf("Qapla'\nleghlaHchu'be'chugh mIn lo'laHbe' taj jej.\n");
+				break;
+			default:
+				printf("luj\n");
+				break;
+		}
+	}
+	return 0;
+}
---
+++ Progtest/Tester.sh
@@ -0,0 +1,15 @@
+PROG=C:/Users/sukkr/source/repos/Progtest/$1/prog.exe
+TEST=C:/Users/sukkr/source/repos/Progtest/$1/CZE
+for IN_FILE in $TEST/*_in.txt
+  do    
+  REF_FILE=`echo -n $IN_FILE | sed -e 's/_in\(.*\)$/_out_win\1/'`
+  $PROG < $IN_FILE > out.txt
+  if ! diff $REF_FILE out.txt
+  then        
+    echo "Fail: $IN_FILE | $REF_FILE";
+    exit
+  else
+    echo "OK $IN_FILE";
+  fi
+  rm "out.txt"
+done
