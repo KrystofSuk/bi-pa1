e0db251c295aff28260ff60a2c24f76b00fa5d8b
Kryštof Šuk <suk.krystof@gmail.com>
2018-11-24 22:51:22 +0100

    Pivot fail

--- Progtest/CV8-BillboardyB/prog.c
+++ Progtest/CV8-BillboardyB/prog.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <limits.h>
 
+struct point{
+  int value;
+  int amount;
+};
+
 /*Vzato z example http://www.cplusplus.com/reference/cstdlib/qsort/ dokumentaci ke qsortu*/
 int compare (const void * a, const void * b){
   return ( *(int*)a - *(int*)b );
@@ -15,22 +20,42 @@ int compare (const void * a, const void * b){
  * @param cnt is count of positions
  * @return int is 0 for bad input or 1 for good
  */
-int inputs(int pos[], int * cnt)
+int inputs(struct point pos[], int * cnt, int * am)
 {  
   int index = 1;
+  int total = 0;
   printf("Mozna umisteni:\n");
 
-  if(scanf(" { %d", &pos[0])!=1  && pos[0] < 1)
+  if(scanf(" { %d", &pos[0].value)!=1  || pos[0].value < 0)
   {
       return 0;
   }
-  
+  pos[0].amount = 1;
+  total++;
   while(1){
     if(index == 1000000){
       break;
     }
+    int inp = -1;
+    int t = scanf(" , %d",&inp);
+    qsort(pos, index, sizeof(struct point), compare);
 
-    int t = scanf(" , %d",&pos[index]);
+    if(t != 0){
+      total++;
+      int added = 0;
+      for(int i = 0; i < index; i++){
+        if(inp == pos[i].value){
+          pos[i].amount++;
+          added = 1;
+          break;
+        }
+      }
+      if(added == 0){
+        pos[index].value = inp;
+        pos[index].amount = 1;
+        index++;
+      }
+    }
 
     if(t == 0){
       char c;
@@ -41,44 +66,29 @@ int inputs(int pos[], int * cnt)
       }
       break;
     }
-
-    if(pos[index] < 1){
+    if(inp < 0){
         return 0;
     }
-    index++;
+
+
   }
+  *am = total;
   *cnt = index;
   return 1;
 }
 
-/**
- * @brief This function is selecting and counting best billboard positions
- * 
- * @param pos is array of possible positions
- * @param ref is array of best selected positions
- * @param count is number of possible positions 
- * @param dif is maximum distance between two positions
- * @return int is best position count or error (-1)
- */
-int countBillboards(int pos[], int ref[], int count, int dif){
-  int lastDif = 0;
-  int cnt = 0;
-
-  for(int i = 1; i < count; i++){
-    
-    if(pos[i]-pos[i-1] > dif)
-      return -1;
-    
-    lastDif += pos[i]-pos[i-1];
-    printf("%d-%d / %d %d\n",pos[i],pos[i-1], lastDif, dif);
-    if(lastDif > dif){  
-      cnt++;
-      ref[cnt+2] = pos[i-1];
-      lastDif = pos[i]-pos[i-1];
+int getPivoted(struct point pos[], int count, double pivot){
+  int dif = INT_MAX;
+  int val = -1;
+  for(int i = 1; i < count-1; i++){
+    if(abs(pivot - (double)pos[i].value) < dif){
+      val = pos[i].value;
+      dif = abs(pivot - pos[i].value);
+    }else{
+      break;
     }
-    
   }
-  return cnt;
+  return val;
 }
 
 /**
@@ -90,70 +100,49 @@ int countBillboards(int pos[], int ref[], int count, int dif){
  * @param req is required number of billboards
  * @return int is minimum lenght between bilboards  
  */
-int distanceBillboards(int pos[], int ref[], int count, int req){
+int distanceBillboards(struct point pos[], int ref[], int count, int req){
   int dist = -1;
-  int f = -1;
-
-  for(int i = 0; i < count; i++)
-    ref[i] = INT_MAX;
-
-  ref[0]=pos[0];
-  ref[1]=pos[count-1];
   
-  if(req == 2)
-    return ref[1]-ref[0];
+  double pivot = ((double)(pos[count-1].value-pos[0].value)/(req-1.0));
+  
+  ref[0]=pos[0].value;
+  int index= 1;
+  for(int i = 1; i < req -1; i++){
 
-  for(int i =  pos[count-1]; i >= 0; i--){
-    for(int t = 0; t < count; t++)
-      ref[t] = INT_MAX;
+    double next = pivot*(i)+(double)pos[0].value;
+    int tmp = getPivoted(pos, count, next);
     
-    ref[0]=pos[0];
-    ref[1]=pos[count-1];
-    
-    int res = countBillboards(pos, ref, count, i);
-    printf("-----\n");
-    for(int x = 0; x < res; x++)
-      printf("%d\n",ref[x]);
-    printf("-----\n");
-    if(res > 0)
-      res+=2;
-    
-    if(res == req){
-      f=1;
+    if(ref[index-1]!=tmp){
+      ref[index] = tmp;
+      index++;
     }
   }
-
-  if(f==1){
-    int smal = ref[1]-ref[0];
-
-    qsort(ref, count, sizeof(int), compare);    
-       
-      
-    for(int x = 1; x < req; x++)
-      if(smal > ref[x]-ref[x-1])
-        smal = ref[x]-ref[x-1];
-    
-    dist = smal;
+  index++;
+  ref[index-1]=pos[count-1].value;
+  int smal = ref[index-1]-ref[0];
+          
+  for(int x = 1; x < index; x++){
+    if(smal > ref[x]-ref[x-1]){
+      smal = ref[x]-ref[x-1];
+    }
   }
-  
+      
+  dist = smal;
   return dist;
 }
 
 int main (void){  
-  static int pos [1000000];
+  static struct point pos [1000000];
   static int refPos [1000000];
   int count = 0;
+  int totalAmount = 0;
 
 
-  if(inputs(pos, &count)==0){    
+  if(inputs(pos, &count, &totalAmount)==0 || count == 0){    
     printf("Nespravny vstup.\n");
     return 0;
   }
 
-  qsort(pos, count, sizeof(int), compare);
-  for(int i =0; i< count;i++){
-   // printf("%d , ", pos[i]);
-  }
   printf("Pocet billboardu:\n");
 
   while(1){
@@ -165,12 +154,17 @@ int main (void){
       }
       break;
     }
-
+    if(req < 0){
+      printf("Nespravny vstup.\n");
+      break;
+    }
     if(req<=1 ){
       printf("Vzdalenost: inf\n");
     }
-    else if(req > count){
+    else if(req > totalAmount){
       printf("N/A\n");
+    }else if(req > count){
+      printf("Vzdalenost: 0\n");
     }else{
       int len = distanceBillboards(pos,refPos,count,req);
       printf("Vzdalenost: %d\n", len);
